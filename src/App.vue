fe877dcc7294af934d6d903c4501b83af403c719
Hicham El Haddari <h.elhadda@gmail.com>
2019-11-07 19:08:10 +0100

    design + homepage + api postman = done | api testée avec postman reste test avec page

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,25 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+  <v-app app class ="app" min-height= "0" wrap>
+    <toolbarCore />
+      <v-content min-height= "0" >
+        <router-view></router-view>
+      </v-content>
+    <footerCore />
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import toolbarCore from './components/core/toolbarCore'
+import footerCore from './components/core/footerCore'
 
 export default {
   name: 'App',
-
   components: {
-    HelloWorld,
+    toolbarCore,
+    footerCore
   },
-
   data: () => ({
     //
-  }),
-};
+  })
+}
 </script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+import router from './router/router.js'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
